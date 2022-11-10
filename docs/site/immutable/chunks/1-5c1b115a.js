d66dde413d67df003bde6fb8ad5d5ef3b1c874e0
Sergio Rodríguez Gómez <46205341+srsergiorodriguez@users.noreply.github.com>
2022-11-09 23:16:26 -0500

    intento de nueva instal

---
+++ docs/site/immutable/chunks/1-5c1b115a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-89de00e4.js";export{t as component};
---
+++ docs/site/immutable/chunks/4-1d99be12.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/proyecto/_page.svelte-1b739cc8.js";export{t as component};
